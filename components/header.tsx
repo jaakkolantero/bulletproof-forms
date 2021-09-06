f26e5238cb3011d884611173b48e285f39ba1a28
Tero Jaakkola <tero.jaakkola@profitsoftware.com>
2021-09-06 21:25:43 +0300

    clean layout for demo

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <h1 className="text-6xl font-bold">
+      Bulletproof Forms{" "}
+      <a className="text-blue-600" href="https://nextjs.org">
+        Part 1!
+      </a>
+    </h1>
+  );
+};
---
+++ components/subHeader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const SubHeader = () => {
+  return (
+    <p className="mt-3 text-2xl">
+      Simple example using RHF and ZOD with typescript{" "}
+      <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+        pages/index.tsx
+      </code>
+    </p>
+  );
+};
