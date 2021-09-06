34dc8abdb0362aeb381ff846ce049ff09b5daac1
Tero Jaakkola <tero.jaakkola@profitsoftware.com>
2021-09-06 21:57:46 +0300

    add simple form

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,31 @@ import Head from "next/head";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import { SubHeader } from "../components/subHeader";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+
+const userFormSchema = z.object({
+  firstname: z.string(),
+  lastname: z.string(),
+  fullname: z.string(),
+});
+
+type UserFormSchema = z.infer<typeof userFormSchema>;
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserFormSchema>({
+    resolver: zodResolver(userFormSchema),
+  });
+
+  useEffect(() => console.table(errors), [errors]);
+  const onSubmit = (data: UserFormSchema) => console.table(data);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -16,10 +39,43 @@ export default function Home() {
         <SubHeader />
 
         <div className="flex flex-wrap items-center justify-around w-full max-w-3xl mt-6">
-          <form autoComplete="new-password" className="flex flex-col space-y-2">
-            <input type="text" name="firstname" />
-            <input type="text" name="lastname" />
-            <input type="text" name="fullname" />
+          <form autoComplete="off" className="flex flex-col space-y-2">
+            <div className="">
+              <label
+                className="block mb-2 text-sm font-bold text-gray-700"
+                htmlFor="firstname"
+              >
+                Firstname
+              </label>
+              <input {...register("firstname")} type="text" name="firstname" />
+            </div>
+            <div className="">
+              <label
+                className="block mb-2 text-sm font-bold text-gray-700"
+                htmlFor="lastname"
+              >
+                Lastname
+              </label>
+              <input {...register("lastname")} type="text" name="lastname" />
+            </div>
+            <div className="">
+              <label
+                className="block mb-2 text-sm font-bold text-gray-700"
+                htmlFor="fullname"
+              >
+                Fullname
+              </label>
+              <input
+                {...register("fullname")}
+                readOnly
+                defaultValue="asdasd"
+                type="text"
+                name="fullname"
+              />
+            </div>
+            <button type="submit" onClick={handleSubmit(onSubmit)}>
+              Submit
+            </button>
           </form>
         </div>
       </main>
