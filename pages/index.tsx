205cc7196a921d67835ad555ec077c4124571dbb
Tero Jaakkola <tero.jaakkola@profitsoftware.com>
2021-09-06 21:02:37 +0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,25 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Bulletproof Forms</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Bulletproof Forms{" "}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            Part 1!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          Simple example using RHF and ZOD with typescript{" "}
           <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
+            pages/index.tsx
           </code>
         </p>
 
@@ -73,10 +73,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
-  )
+  );
 }
